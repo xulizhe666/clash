67afe1edfe767d3a297bc434bcdcab213598e2bc
xulizhe666 <68450475+xulizhe666@users.noreply.github.com>
2023-04-28 15:13:53 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,36 +1,16 @@
-const http = require('http');
-const https = require('https');
-const { URL } = require('url');
+/**
+ * @supported 9E1BBA07
+ */
 
-const url = new URL('https://api.m.jd.com/client.action?functionId=newUserInfo');
+const url = $request.url
 
-const options = {
-  hostname: url.hostname,
-  path: url.pathname + url.search,
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-    'Referer': 'https://m.jd.com/',
-    'Cookie': '这里替换为您的Cookie'
-  }
-};
-
-const req = https.get(options, (res) => {
-  let data = '';
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
-  res.on('end', () => {
-    const cookie = res.headers['set-cookie'][0];
-    const matches = cookie.match(/pt_key=(.*?);.*pt_pin=(.*?);/);
-    const pt_key = matches[1];
-    const pt_pin = matches[2];
-    console.log(`pt_key: ${pt_key}, pt_pin: ${pt_pin}`);
-    // 在此处替换为您的通知代码
-  });
-});
-
-req.on('error', (e) => {
-  console.error(e);
-});
-
-req.end();
+if (url.indexOf('https://mars.jd.com/log/sdk') != -1) {
+  const cookie = $request.headers['Cookie']
+  const pt_key = cookie.match(/pt_key=([^;]+)/)[1]
+  const pt_pin = cookie.match(/pt_pin=([^;]+)/)[1]
+  const newCookie = `pt_key=${pt_key};pt_pin=${pt_pin};`
+  $notify('成功获取到Cookie', '新的京东Cookie已生成', `Cookie: ${newCookie}`)
+  $done({ headers: { 'Cookie': newCookie } })
+} else {
+  $done({})
+}
