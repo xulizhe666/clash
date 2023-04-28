727e098a2ea4dd24a02c02d8546b872027abb2be
xulizhe666 <68450475+xulizhe666@users.noreply.github.com>
2023-04-28 15:16:55 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -9,7 +9,7 @@ if (url.indexOf('https://mars.jd.com/log/sdk') != -1) {
   const pt_key = cookie.match(/pt_key=([^;]+)/)[1]
   const pt_pin = cookie.match(/pt_pin=([^;]+)/)[1]
   const newCookie = `pt_key=${pt_key};pt_pin=${pt_pin};`
-  $notify('成功获取到Cookie', '新的京东Cookie已生成', `Cookie: ${newCookie}`)
+  $notify('成功获取到Cookie', '新的京东Cookie已生成', `${newCookie}`)
   $done({ headers: { 'Cookie': newCookie } })
 } else {
   $done({})
